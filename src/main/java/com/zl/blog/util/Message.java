4986a1f556f1d0465ad591c9757c20711047ecaa
qixiao <zhengliang008@qq.com>
2019-11-09 17:25:37 +0800

    登录API接口实现”

--- src/main/java/com/zl/blog/util/Message.java
+++ src/main/java/com/zl/blog/util/Message.java
@@ -1,4 +1,7 @@
 package com.zl.blog.util;
 
 public class Message {
+    public static String SIGN_IN_SUCCESS = "登录成功";
+    public static String PASSWORD_ERROR = "密码错误";
+    public static String MOBILE_NOT_FOUND = "手机号不存在";
 }
